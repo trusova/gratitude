0f9e3cb2bf35a457fd76cd676ecfa03411d937dc
Olga <trusova@gmail.com>
2011-12-14 20:14:33 -0800

    removed fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 QuestionApp::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users
 
   root :to => "blessings#new"
 
